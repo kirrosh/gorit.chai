45dcbe6c30df83a222f71daf7889b490ec80aa46
Kirill Koshelev <olegartor7@gmail.com>
2021-12-12 12:16:43 +0300

    force build

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -7,6 +7,7 @@ import { faunaClient } from '@/lib/fauna'
 
 export default NextAuth({
 	providers: [
+		
 		SpotifyProvider({
 			clientId: process.env.SPOTIFY_CLIENT_ID!,
 			clientSecret: process.env.SPOTIFY_CLIENT_SECRET!,
