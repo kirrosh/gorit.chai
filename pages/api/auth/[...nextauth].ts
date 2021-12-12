2f2ecb2c9f17a09e9e99ecb8e5eb668d9e70552f
Kirill Koshelev <olegartor7@gmail.com>
2021-12-12 12:30:47 +0300

    added secret

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -6,8 +6,8 @@ import { FaunaAdapter } from '@next-auth/fauna-adapter'
 import { faunaClient } from '@/lib/fauna'
 
 export default NextAuth({
+	secret: '+TE3l2VwO516uji0JqtiwydsonuijBIuiYFnm1TO3GE=',
 	providers: [
-		
 		SpotifyProvider({
 			clientId: process.env.SPOTIFY_CLIENT_ID!,
 			clientSecret: process.env.SPOTIFY_CLIENT_SECRET!,
